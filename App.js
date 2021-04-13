e6afe3ba46e0493c7903aa72ac199c055adca290
Aayush Rajput <aayush.coer15@gmail.com>
2021-04-13 12:28:13 +0530

    VAD init

--- App.js
+++ App.js
@@ -1,21 +1,184 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity,
+  PermissionsAndroid,
+  ToastAndroid,
+  ActivityIndicator
+} from 'react-native';
+import { WHITE, GRADBLUE, GREEN, GRADRED } from './utils/COLORS';
+import LinearGradient from 'react-native-linear-gradient';
+import Icon  from 'react-native-vector-icons/FontAwesome';
+
+import LiveAudioStream from 'react-native-live-audio-stream'; //The main library
+import { Buffer } from 'buffer';
+
+//Record and storage permission Permissions..
+async  function askPermission(){
+  if(Platform.OS === 'android'){
+      try{
+          const granted = await PermissionsAndroid.request(
+              PermissionsAndroid.PERMISSIONS.RECORD_AUDIO, {
+              title: 'Permission request to use audio',
+              message: 'Give permission to use auido to record',
+              buttonPositive: 'OK'
+          });
+          if (granted === PermissionsAndroid.RESULTS.GRANTED){
+              //console.log("You can now perform audio recording.")
+          }
+          else{
+              console.log("Permission Denied.")
+              return;
+          }
+      }
+      catch(e){
+          console.warn("Permission Error: ", e)
+          return;
+      }
+  }
+
+  if(Platform.OS === 'android'){
+    try{
+        const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE, {
+            title: 'Permission to read storage',
+            message: 'Give permission to use storage to read',
+            buttonPositive: 'OK'
+        });
+        if (granted === PermissionsAndroid.RESULTS.GRANTED){
+           // console.log("You can use storage.")
+        }
+        else{
+            console.log("Permission Denied.")
+            return;
+        }
+    }
+    catch(e){
+        console.warn("Permission Error: ", e)
+        return;
+    }
+}
+
+  if(Platform.OS === 'android'){
+      try{
+          const granted = await PermissionsAndroid.request(
+              PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE, {
+              title: 'Permission to write storage',
+              message: 'Give permission to use storage to write',
+              buttonPositive: 'OK'
+          });
+          if (granted === PermissionsAndroid.RESULTS.GRANTED){
+              //console.log("You can use storage.")
+          }
+          else{
+              console.log("Permission Denied.")
+              return;
+          }
+      }
+      catch(e){
+          console.warn("Permission Error: ", e)
+          return;
+      }
+  }
+}
 
 export default function App() {
+  const [recording, setRecording] = useState(false);
+  const [voiceActivity, setVoiceActivity] = useState(false);
+
+  const options = {
+    sampleRate: 32000,  // default is 44100 but 32000 is adequate for accurate voice recognition
+    channels: 1,        // 1 or 2, default 1
+    bitsPerSample: 16,  // 8 or 16, default 16
+    audioSource: 6,     // android only (see below)
+    bufferSize: 4096    // default is 2048
+  };
+  
+  LiveAudioStream.init(options);
+  LiveAudioStream.on('data', data => {
+    var chunk = Buffer.from(data, 'base64');
+    console.log(chunk)
+  });
+
+   //As the value of recording changed we will start or stop the recording
+   useEffect(() => {
+    console.log("Recording value(useEffect): ", recording)
+    if (recording){
+      console.log("Start..")
+      LiveAudioStream.start()
+    }
+    else{
+      console.log("..Stop")
+      LiveAudioStream.stop()
+    }
+  }, [recording])
+
+  useEffect(() => {
+    askPermission();
+  }, [])
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.vadContainer}> 
+        <Text style={styles.voiceActivity}>We will show the voice activity detection here.</Text>
+      </View>
+      <View style={styles.recordButtonContainer}>
+        {/* Make this to toggle betweeen recording and recorded state */}
+        <TouchableOpacity activeOpacity={1} onPress={() => { setRecording(!recording) }}>
+          <LinearGradient colors={(recording)? GRADRED : GRADBLUE} style={styles.recordButton}>
+              <Text style={styles.recordText}>
+              {(recording)?
+                    <Icon name="stop" size={25} color={WHITE}/>:
+                    <Icon name="microphone" size={30} color={WHITE}/>
+                  }
+              </Text>
+          </LinearGradient>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
+const ScreenWidth = Dimensions.get('window').width
+const ScreenHeight = Dimensions.get('window').height
+
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: WHITE,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  }, 
+  vadContainer: {
+    flex: 0.7,    
     justifyContent: 'center',
+    alignItems: 'center',
   },
+  voiceActivity: {
+    color: WHITE,
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    width: ScreenWidth/1.5,
+  },
+  recordButtonContainer: {
+    flex: 0.3,    
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  recordButton: {
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    padding: 10,
+    borderRadius: 150,
+    width: 75,
+    height: 75,
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: WHITE
+  }
 });
---
+++ utils/COLORS.js
@@ -0,0 +1,5 @@
+export const WHITE = '#F3F2FF'
+export const GRADBLUE = ['#43D4FF', '#38ABFD', '#2974FA']
+export const GREY = '#919191'
+export const GRADRED = ['#FF0000', '#FF5436', '#FF8336' ]
+export const GREEN = '#00E33D'
